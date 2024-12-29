358d2440bbf37055ff95f54aaea9d7f81d4b646b
Pratyush Vel Shankar <pratyushvel@outlook.com>
2024-12-29 18:29:05 +0530

    beautiful landing page design done

--- app/page.tsx
+++ app/page.tsx
@@ -4,58 +4,18 @@ import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    ctx.strokeStyle = "#E1E1E1";
-    ctx.lineWidth = 15;
-    ctx.shadowBlur = 5;
-    ctx.shadowColor = "#E1E1E1";
-
-    let x = 0;
-    const drawECG = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      ctx.moveTo(x, canvas.height / 7);
-
-      for (let i = 0; i < x; i++) {
-        const y = canvas.height / 7 + Math.sin(i * 0.0075) * 30 + Math.sin(i * 0.02) * 20 + Math.sin(i * 0.02) * 5;
-        ctx.lineTo(i, y);
-      }
-
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(canvas.width - x, canvas.height - canvas.height / 7);
-
-      for (let i = canvas.width; i > canvas.width - x; i--) {
-        const y = canvas.height - canvas.height / 7 + Math.sin(i * 0.01) * 30 + Math.sin(i * 0.02) * 25 + Math.sin(i * 0.035) * 5;
-        ctx.lineTo(i, y);
-      }
-
-      ctx.stroke();
-
-      x += 6;
-      if (x < canvas.width) {
-        requestAnimationFrame(drawECG);
-      }
-    };
-
-    drawECG();
-  }, []);
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh"}}>
-      <canvas ref={canvasRef} style={{ position: "absolute", top: 0, left: 0 }} />
-      <div style={{ position: "relative", zIndex: 1, width: "100%", height: "100%"}}>
-        <h1 className="title" style={{ position: "absolute", top: "45%", left: "50%", transform: "translate(-50%, -50%)"}}>pulse.</h1>
+    <div className="background" style={{ position: "relative", width: "100%", height: "100vh"}}>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", position: "absolute", width: "100%", height: "10%", zIndex: 1, padding: "0 20px" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Image src='/logo.png' width={50} height={50} quality={100} alt="logo"/>
+          <span style={{ marginLeft: "10px", fontSize: "1.5rem" }} className="logotext">pulse.</span>
+        </div>
+        <button style={{ padding: "10px 20px"}} className="start_button">get started</button>
+      </div>
+      <div style={{ position: "relative", zIndex: 2, width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <h1 className="title" style={{transform: "translate(-50%, -50%)", zIndex: 2}}>stop planning, <br/> start doing.</h1>
+        <div style={{ zIndex: 2 }} className="startscroll">scroll to get started <br/> ▼</div>
       </div>
     </div>
   );
