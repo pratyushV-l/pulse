dbcbce4a0dc2567ec519ef68d19d9798fe2587ef
Pratyush Vel Shankar <pratyushvel@outlook.com>
2024-12-30 13:52:48 +0530

    updated login-signup page

--- app/login-signup/page.tsx
+++ app/login-signup/page.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 export default function LoginSignup() {
 
   const handleGoogleSignIn = () => {
-    signIn("google");
+    signIn("google", { redirect_uri: "https://pulsev1.me/" });
   };
 
   const handleGitHubSignIn = () => {
